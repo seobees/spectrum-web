4db74747bded75ffec862888ae3c79880842675b
igorxciv <icheliadinski@gmail.com>
2020-09-04 19:29:31 -0400

    add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // parserPreset: {
+  //   parserOpts: {
+  //     headerPattern: /^(\w+)\:\s(ISSUE)\-(\d+)\:\s(.+)$/,
+  //     headerCorrespondence: ['type', 'scope', 'ticket', 'subject']
+  //   }
+  // },
+  rules: {
+    'type-enum': [
+			2,
+			'always',
+			[
+				'chg',
+				'new',
+				'fix',
+			]
+    ],
+    'scope-case': [2, 'always', 'upper-case'],
+  }
+};
